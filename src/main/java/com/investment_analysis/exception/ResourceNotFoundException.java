a7cd6c243e01beecae61da5a60227ef3483c1300
Tanish <yesebuadp@gmail.com>
2025-06-17 12:44:56 +0530

    Added Resource not found exception

---
+++ src/main/java/com/investment_analysis/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.investment_analysis.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public  ResourceNotFoundException(String msg){
+        super(msg);
+
+    }
+}
