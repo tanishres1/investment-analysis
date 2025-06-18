337d58f96f7fb2583197071c7097b9522340e1f9
Tanish <yesebuadp@gmail.com>
2025-06-18 09:29:22 +0530

    Added changes entity class both client and Investment

--- src/main/java/Reposirtory/InvestmentRepo.java
+++ src/main/java/Reposirtory/InvestmentRepo.java
@@ -7,6 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface InvestmentRepo extends JpaRepository<Investment, Long> {
-   public  List<Investment> findByClientId(Long clientId);
 
 }
---
+++ src/main/java/model/Client.java
@@ -0,0 +1,24 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long clientId;
+    private String clientName;
+    private String email;
+}
